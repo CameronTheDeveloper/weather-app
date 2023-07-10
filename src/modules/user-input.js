62a370a89a20cfcafca2b8211c0fc0e31b4afc85
CameronTheDeveloper <Cameronposey@outlook.com>
2023-07-10 00:48:10 -0700

    Add weatherForm submit listener

--- src/modules/user-input.js
+++ src/modules/user-input.js
@@ -1,1 +1,10 @@
 const weatherForm = document.querySelector('#weather-form');
+
+const registerLocationSubmit = () => {
+    weatherForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        console.log('Submitted');
+    });
+};
+
+export { registerLocationSubmit };
