d341011569ff43dbc601768bfd768cfc426da1c1
CameronTheDeveloper <Cameronposey@outlook.com>
2023-07-10 02:36:49 -0700

    Add setLocation, getLocationInput functions

--- src/modules/user-input.js
+++ src/modules/user-input.js
@@ -1,16 +1,35 @@
 const weatherForm = document.querySelector('#weather-form');
+const locationInput = document.querySelector('#location-input');
 let location = 'london';
 
-//Check syntax
+const setLocation = (newLocation) => {
+    location = newLocation;
+};
+
 const getLocation = async () => {
     return location;
+};
+
+const getLocationInput = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const newLocation = locationInput.value;
+            resolve(newLocation);
+        }, 500);
+    })
+        .then((newLocation) => {
+            setLocation(newLocation);
+            return newLocation;
+        })
+        .catch((e) => {
+            console.log('Location Input Error: ', e);
+            throw e;
+        });
 };
 
 const registerLocationSubmit = () => {
     weatherForm.addEventListener('submit', (event) => {
         event.preventDefault();
-        getLocation();
-        console.log('Submitted');
     });
 };
 
