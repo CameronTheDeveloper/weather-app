18c3d6894e6b47c4347671e574a0829dba059490
CameronTheDeveloper <Cameronposey@outlook.com>
2023-07-11 00:17:32 -0700

    Add getTempF function

--- src/modules/api.js
+++ src/modules/api.js
@@ -2,12 +2,21 @@ import { getLocation } from "./user-input";
 
 const weatherURL = 'http://api.weatherapi.com/v1/current.json?key=32832b2586e74bf3be920337230607&q=';
 
+const getTempF = async (weatherData) => {
+    try {
+        const temp = weatherData.current.temp_f;
+        return temp;
+    } catch (e) {
+        console.log('Error getting temperature: ', e);
+        throw (e);
+    }
+};
+
 const fetchWeather = async (location) => {
     try {
         const weather = await fetch(weatherURL + location, { mode: 'cors' });
 
         const weatherData = await weather.json();
-        console.log(location);
         return weatherData;
     }
     catch (e) {
@@ -19,7 +28,7 @@ const fetchWeather = async (location) => {
 const manageWeatherData = async () => {
     const location = getLocation();
     const weatherData = await fetchWeather(location);
-    console.log(weatherData);
+    const tempF = await getTempF(weatherData);
     //Process weatherData json
 
 };
